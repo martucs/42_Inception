a1d989a1ef1ecb698a4644acf72076143a918d2b
martalc <martalc@debianInception.loc>
2025-10-09 23:10:00 +0200

    added host env variable

--- requirements/wordpress/wordpress_init.sh
+++ requirements/wordpress/wordpress_init.sh
@@ -3,7 +3,7 @@
 if [ -f wp-config.php ]; then return 0; fi
 
 wp core download --allow-root
-wp config create --dbname=$DATABASE_NAME --dbuser=$DATABASE_USER_NAME --dbpass=$DATABASE_USER_PASSWORD --allow-root
+wp config create --dbname=$DATABASE_NAME --dbuser=$DATABASE_USER_NAME --dbpass=$DATABASE_USER_PASSWORD --dbhost=$DATABASE_HOST --allow-root
 wp core install --url=$DOMAIN_NAME --title="$WORDPRESS_TITLE" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --allow-root
 wp user create $WORDPRESS_AUTHOR_USER $WORDPRESS_AUTHOR_EMAIL --role=author --user_pass=$WORDPRESS_AUTHOR_PASSWORD --allow-root
 
