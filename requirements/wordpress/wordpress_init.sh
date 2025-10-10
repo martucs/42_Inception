5ee81e34b5e16bd8abbae2bdfc76261031e6c164
martalc <martalc@debianInception.loc>
2025-10-10 19:43:16 +0200

    wordpress works with env through docker run

--- requirements/wordpress/wordpress_init.sh
+++ requirements/wordpress/wordpress_init.sh
@@ -2,9 +2,27 @@
 
 if [ -f wp-config.php ]; then return 0; fi
 
-wp core download --allow-root
-wp config create --dbname=$DATABASE_NAME --dbuser=$DATABASE_USER_NAME --dbpass=$DATABASE_USER_PASSWORD --dbhost=$DATABASE_HOST --allow-root
-wp core install --url=$DOMAIN_NAME --title="$WORDPRESS_TITLE" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --allow-root
+until wp core download --allow-root; do
+	echo "Waiting for network..."
+	sleep 3
+done
+
+echo "waiting for MariaDB to accept connections"
+while ! exec 3<>/dev/tcp/$DATABASE_HOST/3306; do
+	sleep 3
+done
+echo "MariaDB is ready!"
+wp config create --dbname=$DATABASE_NAME \
+		 --dbuser=$DATABASE_USER_NAME \
+		 --dbpass=$DATABASE_USER_PASSWORD \
+		 --dbhost=$DATABASE_HOST \
+		 --allow-root
+wp core install --url=$DOMAIN_NAME \
+		--title="$WORDPRESS_TITLE" \
+		--admin_user=$WORDPRESS_ADMIN_USER \
+		--admin_password=$WORDPRESS_ADMIN_PASSWORD \
+		--admin_email=$WORDPRESS_ADMIN_EMAIL \
+		--allow-root
 wp user create $WORDPRESS_AUTHOR_USER $WORDPRESS_AUTHOR_EMAIL --role=author --user_pass=$WORDPRESS_AUTHOR_PASSWORD --allow-root
 
 exec "$@"
